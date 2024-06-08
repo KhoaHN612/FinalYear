45d2abf8a60e23cd97a8b2f1dfbb09f96ead103f
KhoaHN612 <khoahnvtihuy@gmail.com>
2024-06-08 23:55:34 +0700

    Create Player Agent, Create Animator and Animations

---
+++ Assets/_Scripts/AgentAnimation.cs
@@ -0,0 +1,75 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AgentAnimation : MonoBehaviour
+{
+    private Animator animator;
+
+    private void Awake()
+    {
+        animator = GetComponent<Animator>();
+
+    }
+
+    public void PlayAnimation(AnimationType animationType)
+    {
+        switch (animationType)
+        {
+            case AnimationType.die:
+                break;
+            case AnimationType.hit:
+                break;
+            case AnimationType.idle:
+                Play("Idle");
+                break;
+            case AnimationType.attack:
+                break;
+            case AnimationType.run:
+                Play("Run");
+                break;
+            case AnimationType.jump:
+                Play("Jump");
+                break;
+            case AnimationType.fall:
+                Play("Fall");
+                break;
+            case AnimationType.climb:
+                Play("Climbing");
+                break;
+            case AnimationType.land:
+                break;
+            default:
+                break;
+        }
+    }
+
+    internal void StopAnimation()
+    {
+        animator.enabled = false;
+    }
+
+    internal void StartAnimation()
+    {
+        animator.enabled = true;
+    }
+
+    public void Play(string name)
+    {
+        animator.Play(name, -1, 0f);
+    }
+}
+
+public enum AnimationType
+{
+    die,
+    hit,
+    idle,
+    attack,
+    run,
+    jump,
+    fall,
+    climb,
+    land
+}
---
+++ Assets/_Scripts/States/JumpState.cs
@@ -0,0 +1,55 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class JumpState : MovementState
+{
+    private bool jumpPressed = false;
+    [SerializeField]
+    protected State ClimbState;
+
+    protected override void EnterState()
+    {
+        agent.animationManager.PlayAnimation(AnimationType.jump);
+        movementData.currentVelocity = agent.rb2d.velocity;
+        movementData.currentVelocity.y = agent.agentData.jumpForce;
+        agent.rb2d.velocity = movementData.currentVelocity;
+        jumpPressed = true;
+    }
+
+    protected override void HandleJumpPressed()
+    {
+        jumpPressed = true;
+    }
+
+    protected override void HandleJumpReleased()
+    {
+        jumpPressed = false;
+    }
+
+    public override void StateUpdate()
+    {
+        ControlJumpHeight();
+        CalculateVelocity();
+        SetPlayerVelocity();
+        if (agent.rb2d.velocity.y <= 0)
+        {
+            agent.TransitionToState(FallState);
+        }
+        else if (agent.climbingDetector.CanClimb && Mathf.Abs(agent.agentInput.MovementVector.y) > 0)
+        {
+            agent.TransitionToState(ClimbState);
+        }
+    }
+
+    private void ControlJumpHeight()
+    {
+        if(jumpPressed == false)
+        {
+            movementData.currentVelocity = agent.rb2d.velocity;
+            movementData.currentVelocity.y += agent.agentData.lowJumpMultiplier *Physics2D.gravity.y * Time.deltaTime;
+            agent.rb2d.velocity = movementData.currentVelocity;
+        }
+    }
+}
